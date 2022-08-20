9c7cc206d989f2f6d3fbda127f5b416ef32ae3be
Larissa Cooper <52721788+LarissaCoop@users.noreply.github.com>
2022-08-19 20:11:54 -0400

    Add files via upload

---
+++ var elem = document.getElementsByTagName.js
@@ -0,0 +1,31 @@
+var elem = document.getElementsByTagName("iframe");
+
+/* When the openFullscreen() function is executed, open the video in fullscreen.
+Note that we must include prefixes for different browsers, as they don't support the requestFullscreen property yet */
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) {
+    /* Safari */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) {
+    /* IE11 */
+    elem.msRequestFullscreen();
+  }
+}
+
+openFullscreen();
+/* Close fullscreen */
+function closeFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.webkitExitFullscreen) {
+    /* Safari */
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) {
+    /* IE11 */
+    document.msExitFullscreen();
+  }
+}
+HTMLElementObject.exitFullscreen();
+closeFullscreen();
